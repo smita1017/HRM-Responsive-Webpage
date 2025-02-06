97c81ee88ba0f1eb2dea12714414f1e99ec9332d
smita1017 <smita126@outlook.com>
2025-02-06 10:07:35 +0530

    6-feb-2025

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import "@fontsource/montserrat"; // Default font import
-import "@fontsource/montserrat/400.css"; // Regular weight
-import "@fontsource/montserrat/700.css"; // Bold weight
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
